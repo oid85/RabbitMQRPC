b8bfe15971af1ac9820e72a43999f8a200723332
oid85 <oid85@mail.ru>
2024-04-03 09:54:17 +0700

    TASK-03 Completed

---
+++ RabbitMQRPC/RabbitMQRPC.Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await InvokeAsync("hi there!");
+
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string input)
+    {
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+
+        using var rpcClient = new RpcClient();
+
+        // Помещаем вызов в Task
+        _ = Task.Run(async () => await rpcClient.CallAsync(input, cancellationToken));
+
+        // Спустя таймаут отменяем
+        await Task.Delay(1_000, cancellationToken);
+        
+        try
+        {
+            cts.Cancel();
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
